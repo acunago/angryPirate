c2f147c6381475d1cc67c62ac954a981aefa51b0
acunago <49562365+acunago@users.noreply.github.com>
2019-06-21 00:15:14 -0300

    Clouds

---
+++ Assets/Scripts/CloudsScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudsScript : MonoBehaviour
+{
+    private float i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i += Time.deltaTime;
+            transform.Rotate( new  Vector3(0, Mathf.Abs(Mathf.Sin(i)) * Time.deltaTime, 0));
+    }
+}
