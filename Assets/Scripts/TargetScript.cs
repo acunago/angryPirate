07af2f2dcd788d559196ef8b0d8b593783fc4ed9
acunago <49562365+acunago@users.noreply.github.com>
2019-06-21 00:30:11 -0300

    Audios y animaciones

--- Assets/Scripts/TargetScript.cs
+++ Assets/Scripts/TargetScript.cs
@@ -6,13 +6,22 @@ public class TargetScript : MonoBehaviour
 {
 
     public float Points;
-    // Start is called before the first frame update
 
+    private Animator anim;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (transform.GetComponent<Animator>())
+        {
+            anim = transform.GetComponent<Animator>();
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == 9)
         {
             Destroy(gameObject, 1);
+            anim.SetBool("death", true);
         }
 
     }
